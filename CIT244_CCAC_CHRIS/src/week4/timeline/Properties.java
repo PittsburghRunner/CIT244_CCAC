7955d64d68b96ef0751eb45bb23d6127453eeed4
ceckles <ceckles@christosmacbook.fios-router.home>
2019-03-11 19:34:16 -0400

    Fixed Local issue

--- CIT244_CCAC_CHRIS/src/week4/timeline/Properties.java
+++ CIT244_CCAC_CHRIS/src/week4/timeline/Properties.java
@@ -1,5 +1,7 @@
 package week4.timeline;
 
+import java.util.ResourceBundle;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,5 +16,16 @@ public class Properties {
             public static final String TIMELINE_FILE_PATH = "timelinerepository";    
             public static final String TIMELINE_FILE = "TIMELINE";    
             public static final String FILE_SUFFIX = ".xml";    
+
+   private ResourceBundle resourceBundle;
+
+    public Properties(String propertyFilePath) {
+       resourceBundle = ResourceBundle.getBundle(propertyFilePath);
+    }
+
+    public String getString(String key) {
+        return resourceBundle.getString(key);
+
+    }
             
 }
--- CIT244_CCAC_CHRIS/src/week4/timeline/menus/MenuItem.java
+++ CIT244_CCAC_CHRIS/src/week4/timeline/menus/MenuItem.java
@@ -6,9 +6,7 @@
 package week4.timeline.menus;
 
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import week4.timeline.Properties;
 
 /**
  *
@@ -16,39 +14,37 @@ import java.util.ResourceBundle;
  */
 public enum MenuItem {
     
-//    
-//    PRINT_COMPONENTS(1, "components.print", "week4.timeline.TimelineWorld", "printComponents"),
-//    ADD_COMPONENT(2, "component.add", "week4.timeline.TimelineWorld", "addComponent"),
-//    DUPLICATE_COMPONENT(3, "component.duplicate", "week4.timeline.TimelineWorld", "duplicateComponent"),
-//    EDIT_COMPONENT(4, "component.edit", "week4.timeline.TimelineWorld", "editComponent"),
-//    DELETE_COMPONENT(5, "component.delete", "week4.timeline.TimelineWorld", "deleteComponent"),
-//    SAVE_TIMELINE(6, "timeline.save", "week4.timeline.TimelineWorld", "saveTimeline"),
-//    SORT_TIMELINE(7, "timeline.sort", "week4.timeline.TimelineWorld", "sort"),
-//    DONT_SAVE_ON_EXIT(8, "no.save.on.exit", "week4.timeline.TimelineWorld", "setSaveOnExitToFalse"),
-//    EXIT(9, "application.exit", "week4.timeline.TimelineWorld", "setSaveOnExitToFalse");
-//    
+    
+    PRINT_COMPONENTS(1, "components.print", "week4.timeline.TimelineWorld", "printComponents"),
+    ADD_COMPONENT(2, "component.add", "week4.timeline.TimelineWorld", "addComponent"),
+    DUPLICATE_COMPONENT(3, "component.duplicate", "week4.timeline.TimelineWorld", "duplicateComponent"),
+    EDIT_COMPONENT(4, "component.edit", "week4.timeline.TimelineWorld", "editComponent"),
+    DELETE_COMPONENT(5, "component.delete", "week4.timeline.TimelineWorld", "deleteComponent"),
+    SAVE_TIMELINE(6, "timeline.save", "week4.timeline.TimelineWorld", "saveTimeline"),
+    SORT_TIMELINE(7, "timeline.sort", "week4.timeline.TimelineWorld", "sort"),
+    DONT_SAVE_ON_EXIT(8, "no.save.on.exit", "week4.timeline.TimelineWorld", "setSaveOnExitToFalse"),
+    EXIT(9, "application.exit", "week4.timeline.TimelineWorld", "setSaveOnExitToFalse");
+    
     
         
-    PRINT_COMPONENTS(1, "Print Components", "week4.timeline.TimelineWorld", "printComponents"),
-    ADD_COMPONENT(2, "Add Components", "week4.timeline.TimelineWorld", "addComponent"),
-    DUPLICATE_COMPONENT(3, "Duplicate Component", "week4.timeline.TimelineWorld", "duplicateComponent"),
-    EDIT_COMPONENT(4, "Edit Component", "week4.timeline.TimelineWorld", "editComponent"),
-    DELETE_COMPONENT(5, "Delete Component", "week4.timeline.TimelineWorld", "deleteComponent"),
-    SAVE_TIMELINE(6, "Save Timeline", "week4.timeline.TimelineWorld", "saveTimeline"),
-    SORT_TIMELINE(7, "Sort Timeline", "week4.timeline.TimelineWorld", "sort"),
-    DONT_SAVE_ON_EXIT(8, "Don't Save On Exit", "week4.timeline.TimelineWorld", "setSaveOnExitToFalse"),
-    EXIT(9, "Exit Application", "week4.timeline.TimelineWorld", "setSaveOnExitToFalse");
-    
-    
-    
-    static ResourceBundle labels = ResourceBundle.getBundle("labels",Locale.US);
-    
+//    PRINT_COMPONENTS(1, "Print Components", "week4.timeline.TimelineWorld", "printComponents"),
+//    ADD_COMPONENT(2, "Add Components", "week4.timeline.TimelineWorld", "addComponent"),
+//    DUPLICATE_COMPONENT(3, "Duplicate Component", "week4.timeline.TimelineWorld", "duplicateComponent"),
+//    EDIT_COMPONENT(4, "Edit Component", "week4.timeline.TimelineWorld", "editComponent"),
+//    DELETE_COMPONENT(5, "Delete Component", "week4.timeline.TimelineWorld", "deleteComponent"),
+//    SAVE_TIMELINE(6, "Save Timeline", "week4.timeline.TimelineWorld", "saveTimeline"),
+//    SORT_TIMELINE(7, "Sort Timeline", "week4.timeline.TimelineWorld", "sort"),
+//    DONT_SAVE_ON_EXIT(8, "Don't Save On Exit", "week4.timeline.TimelineWorld", "setSaveOnExitToFalse"),
+//    EXIT(9, "Exit Application", "week4.timeline.TimelineWorld", "setSaveOnExitToFalse");
 
     
      int menuItemid;
     String menuItemDescription;
     String methodClass;
     String methodToCall;
+    
+    private static Properties labels = new Properties("week4.timeline.labels");
+
 
     MenuItem(int id, String descr, String mclass, String method) {
         menuItemid = id;
@@ -75,7 +71,7 @@ public enum MenuItem {
 
     @Override
     public String toString() {
-        return menuItemid + " - " + menuItemDescription;
+        return menuItemid + " - " + labels.getString(menuItemDescription);
     }
 
     public static void printEntireMenu() {
@@ -83,14 +79,6 @@ public enum MenuItem {
         for (MenuItem mi : MenuItem.values()) {
             String menuItem = mi.toString();
             System.out.println(menuItem);
-        
-//          Iterator i = labels.keySet().iterator();
-//          
-//          while(i.hasNext()){
-//              System.out.println("Key: " + i.next());
-//          }
-//            System.out.println(menuItem);
-//            System.out.println(labels.getString(menuItem));
         }
     }
 
